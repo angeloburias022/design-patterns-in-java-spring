583f6ed46054747c0a29f2202d35a715428a7b2a
lou <angelo@solutionsresource.com>
2024-10-12 16:12:33 +0800

    [CHANGE] change to generalize project

--- src/main/java/com/srllc/bountiply/designPattern/DesignPatternApplication.java
+++ src/main/java/com/srllc/bountiply/designPattern/DesignPatternApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FactoryPatternApplication {
+public class DesignPatternApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FactoryPatternApplication.class, args);
+		SpringApplication.run(DesignPatternApplication.class, args);
 	}
 
 }
