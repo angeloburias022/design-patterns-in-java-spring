d9da9fc64147a83d0991bee26b8fd67881ce9779
lou <angelo@solutionsresource.com>
2024-10-12 16:16:11 +0800

    [FIX] fix test path

--- src/test/java/com/srllc/bountiply/designPattern/DesignPatternApplicationTests.java
+++ src/test/java/com/srllc/bountiply/designPattern/DesignPatternApplicationTests.java
@@ -1,4 +1,4 @@
-package com.srllc.bountiply.factoryPattern;
+package com.srllc.bountiply.designPattern;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
